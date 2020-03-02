7ab7ed4903f43c3e77f8023e375e84ef167fafec
Vijay <AM.Vijay@gmail.com>
2020-03-02 12:04:25 -0600

    Routing updated with blog component.

--- ng-blogger-application/src/app/blogger/routing/routing.module.ts
+++ ng-blogger-application/src/app/blogger/routing/routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from '../blog/blog.component';
 import { HomeComponent } from '../home/home.component';
 
 const routes: Routes = [
   {path:"",component: HomeComponent},
-  {path:"sqoop-installation-and-example", component: HomeComponent}
+  {path:"how-https-works", component: BlogComponent}
 ];
 
 @NgModule({
