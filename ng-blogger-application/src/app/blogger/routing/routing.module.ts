a6b29d72d029ea25ab2713ad7b9a78fd0f55b016
Vijay <AM.Vijay@gmail.com>
2020-05-23 15:46:46 -0500

    Added Routing for datastructure page.

--- ng-blogger-application/src/app/blogger/routing/routing.module.ts
+++ ng-blogger-application/src/app/blogger/routing/routing.module.ts
@@ -7,6 +7,7 @@ import { PageComponent } from '../page/page.component';
 const routes: Routes = [
   { path: "", component: PageComponent },
   { path: "hadoop", component: PageComponent },
+  { path: "datastructure", component: PageComponent },
   {
     path: "blog",
     children: [
